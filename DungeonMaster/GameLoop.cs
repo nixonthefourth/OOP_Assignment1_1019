aa15f0c3ce68126e4a1d4ea303f9251edb377f55
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-02-19 12:41:34 +0000

    Moving messages of a story from GameLoop.cs to the Story file

--- DungeonMaster/GameLoop.cs
+++ DungeonMaster/GameLoop.cs
@@ -2,18 +2,10 @@ namespace DungeonMaster
 {
     public class GameLoop
     {
-        // Variables
-        public string WelcomeMessage { get; set; }
-        
         // Game Loop
         public void Game()
         {
-            // Set Variables
-            WelcomeMessage = "Welcome to the Dungeon Master! \n \n";
             
-            // Gameloop
-            // Output the Entrance
-            Visuals.DisplayMessage(WelcomeMessage.ToUpper());
         }
     }
 }
