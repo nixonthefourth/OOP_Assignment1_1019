f521eb82c76cfe59d17ab0e3a744b74ea5943cff
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-02-19 14:22:53 +0000

    Created setters for the inventory. Created class Story, which contains all the story methods for the game. When called, game event happens, hence can be consolidated in a singular file.

--- DungeonMaster/Inventory.cs
+++ DungeonMaster/Inventory.cs
@@ -7,17 +7,24 @@ namespace DungeonMaster
         public string WeaponName { get; set; }
         public int WeaponDamage { get; set; }
         
+        // Potions
+        public string PotionName { get; set; }
+        public int PotionDamage { get; set; }
+        
         // Methods
+        // Setters
         // Weapons
         public void SetRoomWeapon(string weaponName, int weaponDamage)
         {
             // Checks for the absence of the parameters
+            // Name Check
             if (weaponName == null || weaponName == "" || weaponName == " ")
             {
                 Console.WriteLine("System Error | Weapon Undefined");
                 System.Environment.Exit(1);
             }
-
+            
+            // Damage Value Check
             else if (weaponDamage == null || weaponDamage == 0)
             {
                 Console.WriteLine("System Error | Damage Undefined");
@@ -29,6 +36,32 @@ namespace DungeonMaster
             {
                 weaponDamage = WeaponDamage;
                 weaponName = WeaponName;
+            }
+        }
+        
+        // Potions
+        public void SetRoomPotion(string potionName, int potionDamage)
+        {
+            // Checks for the absence of the parameters
+            // Name Check
+            if (potionName == null || potionName == "" || potionName == " ")
+            {
+                Console.WriteLine("System Error | Weapon Undefined");
+                System.Environment.Exit(1);
+            }
+            
+            // Damage Value Check
+            else if (potionDamage == null || potionDamage == 0)
+            {
+                Console.WriteLine("System Error | Damage Undefined");
+                System.Environment.Exit(1);
+            }
+
+            // Otherwise, we set the values assigned
+            else
+            {
+                potionName = PotionName;
+                potionDamage = PotionDamage;
             }
         }
     }
--- DungeonMaster/Story.cs
+++ DungeonMaster/Story.cs
@@ -2,6 +2,9 @@ namespace DungeonMaster
 {
     public class Story
     {
-    
+        public void Welcome()
+        {
+            Visuals.DisplayMessage("Welcome to DungeonMaster!".ToUpper());
+        }
     }
 }
