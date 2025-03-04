659dc184491e76c2d23a0dcc264a9b639cb90ea7
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-03-04 12:05:57 +0000

    Creating Test File

---
+++ DungeonExplorer/Testing.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+
+namespace DungeonExplorer
+{
+    public class Testing
+    {
+        /*
+         * METHODS
+         * METHODS
+         * METHODS
+         */
+        
+        // Run Unified Tests
+        public void RunTests()
+        {
+            // Display Message
+            Debug.WriteLine("–––Starting Test Sequence–––");
+            
+            // Player Tests
+        }
+    }
+}
