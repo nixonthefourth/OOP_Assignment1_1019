78d8d789b59c1f4df9390562f8ef139659660073
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-03-04 15:04:22 +0000

    Adding required libraries as well as adding spaces

--- DungeonExplorer/Player.cs
+++ DungeonExplorer/Player.cs
@@ -1,7 +1,14 @@
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DungeonExplorer
 {
+    
     public class Player
     {
         /*
--- DungeonExplorer/Program.cs
+++ DungeonExplorer/Program.cs
@@ -1,5 +1,14 @@
-﻿namespace DungeonExplorer
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
 {
+    
     class Program
     {
         static void Main(string[] args)
