1d112861c4efbc479fece6ab5a2544aeecc6af16
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-04-11 15:32:03 +0100

    Starting the development of the second assignment

--- DungeonExplorer/Program.cs
+++ DungeonExplorer/Program.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-
-
-namespace DungeonExplorer
+﻿namespace DungeonExplorer
 {
     
     class Program
--- DungeonExplorer/Story.cs
+++ DungeonExplorer/Story.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-
-
 namespace DungeonExplorer
 {
     
